fc6582740fec5d6d5fedfcae7a120478e0c3cda2
谢韬 <2016011294@secoder.net>
2018-12-04 19:04:02 +0800

    串口能连

--- proj/Memorizer.vhd
+++ proj/Memorizer.vhd
@@ -50,41 +50,85 @@ end Memorizer;
 
 architecture Behavioral of Memorizer is
 	signal rdn_i, wrn_i: std_logic:= '1';
-	type write_states is (
-		write_init,
-		write_set,
-		write_end
+	signal data_temp: std_logic_vector(15 downto 0);
+	type main_states is (
+		waiting,
+		read0,
+		read1,
+		read2,
+		read3,
+		read4,
+		read5,
+		write0,
+		write1,
+		write2,
+		write3,
+		write4,
+		write5,
+		done
 	);
-	signal write_state: write_states:= write_init;
+	signal write_state: main_states:= waiting;
+	signal read_state: main_states:= waiting;
+	signal state: main_states:= waiting;
 begin
 	rdn <= rdn_i;
 	wrn <= wrn_i;
-	ToRead <= RamData;
 	
-	process(WriteMem, ToWrite, Addr, data_ready)
+	process(RamData, state)
+	begin
+		if (Addr = x"BF01") or (Addr = x"BF00") then
+			case state is
+				when write1 => ToRead <= x"0001";
+				when read1 => ToRead <= x"0002";
+				when others => ToRead <= RamData;
+			end case;
+		else
+			ToRead <= RamData;
+		end if;
+	end process;
+	
+	process(WriteMem, ToWrite, Addr)
 	begin
 		if (Addr = x"BF01") or (Addr = x"BF00") then
 			EN_L <= '1';
 			WE_L <= '1';
 			OE_L <= '1';
-			if (WriteMem = "01") and (tbre = '1') and (tsre = '1') then
-				RamData <= ToWrite;
-				wrn_i <= '0';
-				rdn_i <= '1';
-			elsif (WriteMem = "10") and (data_ready = '1') then
-				RamData <= "ZZZZZZZZZZZZZZZZ";
-				wrn_i <= '1';
-				rdn_i <= '0';
-			else
-				RamData <= "ZZZZZZZZZZZZZZZZ";
-				wrn_i <= '1';
-				rdn_i <= '1';
-			end if;
+			--judge whether to promote state until being able to s/l com
+			case state is
+				when waiting =>
+					state <= write0;
+				when write0 =>
+					if (tsre = '1') and (tbre = '1') then
+						--can write--state change => toread=0x0001
+						state <= write1;
+					else
+						--cannot write
+						state <= read0;
+					end if;
+				when write1 =>
+					--do SW
+					state <= waiting;
+					wrn_i <= '0';
+					RamData <= ToWrite;
+				when read0 =>
+					if (data_ready = '1') then
+						--can read
+						state <= read1;--state change => toread=0x0002
+						RamData <= "ZZZZZZZZZZZZZZZZ";
+					else
+						--cannot read
+						state <= waiting;
+					end if;
+				when read1 =>
+					--do LW
+					state <= waiting;
+					rdn_i <= '0';
+				when others => null;
+			end case;
 		else
 			EN_L <= '0';
 			rdn_i <= '1';
 			wrn_i <= '1';
-			write_state <= write_init;
 			
 			RamAddr <= "00" & Addr;
 
